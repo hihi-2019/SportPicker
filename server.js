67e80a693f4d662e8b1490a60124c8944d2550dc
Student <student@eda-bc-06.localdomain>
2019-10-24 16:16:08 +1300

    add basic server setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+// const rugby = require('./routes/rugby')
+// const football = require('./routes/football')
+// const basketball = require('./routes/basketball')
+// const icehockey = require('./routes/icehockey')
+
+const server = express()
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({extended: false}))
+// server.use('/sports', rugby)
+// server.use('/sports', football)
+// server.use('/sports', basketball)
+// server.use('/sports', icehockey)
+
+
+server.get('/sports', (req, res) => {
+  res.send("hello")
+})
+
+
+
+
+server.get('/', (req, res) => {
+  res.redirect('/puppies')
+})
+
+
+
+module.exports = server
