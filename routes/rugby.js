361e0a1ee869895d7b9dc856d9391e457feff311
aliceals <alice.alsford@gmail.com>
2019-10-25 14:35:21 +1300

    fix links

--- routes/rugby.js
+++ routes/rugby.js
@@ -1,30 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const sportData = require('../sportData')
-const rugby = sportData.rugby
-
+const express = require("express");
+const router = express.Router();
+const sportData = require("../sportData");
+const rugby = sportData.rugby;
 
 // console.log(rugby)
 
-router.get('/rugby', (req, res) =>{
-    const team = {
-        team: rugby,
-        sport: rugby
-    }
-    res.render("./partials/team", team) 
-})
+router.get("/rugby", (req, res) => {
+  const team = {
+    team: rugby,
+    sport: "rugby"
+  };
+  res.render("./partials/team", team);
+});
 
-router.get('/rugby/:id', (req, res) =>{
-    const id = req.params.id
-    const rugbyInfo = {
-        Team: sportData.rugby.find(function(element){
-            return element.id == req.params.id
-        }),
-    }
+router.get("/rugby/:id", (req, res) => {
+  const id = req.params.id;
+  const rugbyInfo = {
+    Team: sportData.rugby.find(function(element) {
+      return element.id == req.params.id;
+    })
+  };
 
-    res.render("partials/bio", rugbyInfo.Team)
-})
+  res.render("partials/bio", rugbyInfo.Team);
+});
 
-
-
-module.exports = router
+module.exports = router;
